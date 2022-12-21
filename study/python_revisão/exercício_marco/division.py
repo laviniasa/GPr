ca174147710ada71e8cc303ca5f2aefb52ea5f99
Lavínia Sá <82878705+laviniasa@users.noreply.github.com>
2022-12-21 09:06:14 -0300

    Update division.py

--- study/python_revisão/exercício_marco/division.py
+++ study/python_revisão/exercício_marco/division.py
@@ -1,10 +1,10 @@
 #1.4 - Uma função deve retornar a divisão do maior número pelo menor número
 
-def division ():
-    l = len(list1) 
-    #list1.sort() 
-    return (list1[l-2])*(list1[l-1])
+def division(list1): 
+    l = len(list1)
+    return max(list1)/min(list1)
     
-list1=[1,2,3,4,5] 
+list1=[2,3,4,5,10] 
 Largest = division(list1) 
 print(division(list1)) 
+
