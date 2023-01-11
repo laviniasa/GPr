7ee19b845b68d6be8b6e1af66a21879b35f5c56d
Lavínia Sá <82878705+laviniasa@users.noreply.github.com>
2023-01-11 14:58:09 -0300

    Update exercicio_13.py

--- study/revisão2/exercicio_13.py
+++ study/revisão2/exercicio_13.py
@@ -1,4 +1,5 @@
-# Ao receber 4 sequencias de números
+#13.1
+#  Ao receber 4 sequencias de números
 # crie uma função que retorne uma matriz, 
 # onde cada sequência é uma lista seja uma coluna, na ordem em que for recebida
 
@@ -14,16 +15,17 @@ def coll (seq1, seq2, seq3, seq4):
         matriz.append(subMatriz)
         count = count + 1
 
-    for linha in seq1, seq2, seq3, seq4:
-        for lugar in linha:
-            print(lugar, end=" - ")
-        print("")
-        
+    return matriz
 
 arrey = [1,2,3,]
 arrey2 = [6,7,8]
 arrey3 = [5,0,4]
-arrey4 = [9,0,1]
+arrey4 = [9,0,1]    
 
 test = coll(arrey, arrey2, arrey3, arrey4)
-print(test)
+
+for linha in test:
+        for lugar in linha:
+            print(lugar, end=" - ")
+        print("")
+
