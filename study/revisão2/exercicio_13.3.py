1e37a44b028f7948bdbd92a5b304f4f9baeba365
Lavínia Sá <82878705+laviniasa@users.noreply.github.com>
2023-01-12 08:29:26 -0300

    Update exercicio_13.3.py

--- study/revisão2/exercicio_13.3.py
+++ study/revisão2/exercicio_13.3.py
@@ -1,17 +1,67 @@
 # Uma função que retorne uma matriz, onde cada sequência é uma lista seja uma linha,
-#  na ordem em que for recebida, adicionada de uma coluna que seja a soma das células de cada linha, 
-# e uma linha que seja a soma das células de cada coluna.
+#na ordem em que for recebida, adicionada de uma coluna que seja a soma das células de cada linha,
+#e uma linha que seja a soma das células de cada coluna.
 
 
-def matriz(m, n):
-    matriz = []
-    for i in range(n):
-        matriz.append([5]*m)
-    for i in range(n):
-        print(matriz[i])
+def matriz (seq1, seq2, seq3, seq4):
 
-n = 5
-m = 5
+   matriz= []  
+   
+   soma = 0
+   for linha in seq1:
+      soma = (soma + linha)
+   seq1.append(soma)
+   matriz.append(seq1)
 
-test = matriz(n,m )
-print(test)
+   soma = 0
+   for linha in seq2:
+      soma = (soma + linha)
+   seq2.append(soma)
+   matriz.append (seq2)
+   
+   soma=0
+   for linha in seq3:
+      soma = (soma + linha)
+   seq3.append(soma)
+   matriz.append (seq3)
+   
+   soma=0
+   for linha in seq4:
+      soma = (soma + linha)
+   seq4.append(soma)
+   matriz.append(seq4)
+
+   arreySomaColl = []
+   somaColl = 0
+   i = 0 
+   j = 0
+   for linha in matriz:
+      for coluna in linha:
+            somaColl = somaColl + matriz[i][j]
+            i= i + 1
+      i = 0
+      arreySomaColl.append(somaColl)
+      somaColl = 0
+      j = j + 1
+
+   matriz.append(arreySomaColl)
+
+   return matriz
+
+arrey = [1,2,3]
+arrey2 = [6,7,8]
+arrey3 = [5,0,4]
+arrey4 = [9,0,1]    
+
+# matriz2 = []
+# matriz2.extend([])
+# matriz2.extend([])
+# matriz2.extend([])
+# matriz2.extend([])
+
+test = matriz (arrey, arrey2, arrey3, arrey4)
+for linha in test:
+   print ("")
+   for lugar in linha:
+      print(  lugar, end=" ")
+   print("")
