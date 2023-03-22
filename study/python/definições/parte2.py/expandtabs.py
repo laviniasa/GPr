26399e8572686fba0f0dee903c8af7f7fe23e140
Lavínia Sá <82878705+laviniasa@users.noreply.github.com>
2023-03-22 09:49:21 -0300

    Delete study/python/definições/parte2.py directory

--- study/python/definições/parte2.py/expandtabs.py
+++
@@ -1,9 +0,0 @@
-txt = "H\te\tl\tl\to"
-
-x =  txt.expandtabs(2)
-
-print(x)
-
-
-# expandtabs() ” que especifica a quantidade de espaço a ser substituído pelo símbolo “\ t” na string.
-
--- study/python/definições/parte2.py/isnumeric.py
+++
@@ -1,9 +0,0 @@
-# O isnumeric()método verifica se todos os caracteres da string são numéricos.
-
-pin = "523"
-
-# checks if every character of pin is numeric 
-print(pin.isnumeric())
-              
-
-# Output: True
